263287454719715eb532ea852bc71f209b8da2a9
Tony <gaohaoqian@gmail.com>
2017-07-20 15:12:41 +1000

    Input accessory

--- InsFire/CommentsController.swift
+++ InsFire/CommentsController.swift
@@ -11,8 +11,11 @@ import UIKit
 class CommentsController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView?.backgroundColor = .red
         
+        navigationItem.title = "Comment"
+        
+        collectionView?.backgroundColor = .red
+        self.hideKeyboardWhenTappedAround()
     }
     
     // every time comments controller is called/presented
@@ -27,13 +30,36 @@ class CommentsController: UICollectionViewController {
         tabBarController?.tabBar.isHidden = false
     }
     
+    // this container holds the input text
+    var containerView: UIView = {
+        let containerView = UIView()
+        containerView.backgroundColor = .white
+        containerView.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        
+        let submitButton = UIButton(type: .system)
+        submitButton.setTitle("Submit", for: .normal)
+        submitButton.setTitleColor(.black, for: .normal)
+        submitButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        submitButton.addTarget(self, action: #selector(handleSubmit), for: .touchUpInside)
+        containerView.addSubview(submitButton)
+        submitButton.anchor(top: containerView.topAnchor, left: nil, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 50, height: 0)
+        
+        let textField = UITextField()
+        textField.placeholder = "Enter Comment"
+        containerView.addSubview(textField)
+        textField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: submitButton.leftAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        return containerView
+    }()
+    
+    // this calls the container
     override var inputAccessoryView: UIView? {
         get {
-            let containerView = UIView()
-            containerView.backgroundColor = .white
-            containerView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 50)
             return containerView
         }
+    }
+    
+    func handleSubmit() {
+        print("Submitting comments")
     }
     
     override var canBecomeFirstResponder: Bool {
